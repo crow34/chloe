f74608f2d0048763894bb05d0719dfcbdb97803b
crow34 <thermal24@gmail.com>
2023-05-11 18:16:21 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pyttsx3
 from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
@@ -7,27 +6,25 @@ from hugchat import hugchat
 
 st.set_page_config(page_title="chloe - a helpful assistant ")
 
-# Text-to-Speech function
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
 # Sidebar contents
 with st.sidebar:
     st.title('🤗💬 chloe')
     st.markdown('''
     ## About
-  this is chloe a personnel assistant to martin wood
-    
-    💡 Note: No API key required!
+    this is martin woods personnel assistant
+    - [my name is chloe)
+    - [i am a personnel assistant from martin wood]
+    - [OpenAssistant/oasst-sft-6-llama-30b-xor] LLM model
+    i have over 30 billion parameters
     ''')
     add_vertical_space(5)
     st.write('Made with ❤️ by martin wood')
 
 # Generate empty lists for generated and past.
+## generated stores AI generated responses
 if 'generated' not in st.session_state:
     st.session_state['generated'] = ["I'm chloe, How may I help you?"]
+## past stores User's questions
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi!']
 
@@ -37,25 +34,27 @@ colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
 # User input
+## Function for taking user provided prompt as input
 def get_text():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
-
+## Applying the user input box
 with input_container:
     user_input = get_text()
 
 # Response output
+## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
     chatbot = hugchat.ChatBot()
     response = chatbot.chat(prompt)
     return response
 
+## Conditional display of AI generated responses as a function of user provided prompts
 with response_container:
     if user_input:
         response = generate_response(user_input)
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response)
-        text_to_speech(response)  # Convert response to speech
         
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
